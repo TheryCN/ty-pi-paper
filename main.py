9b55eaa55470463da25ede8788ec9cf0a729cd57
Théry Charasson <thery.charasson@gmail.com>
2018-10-14 09:36:02 +0200

    Add scheduler

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
-##
- #  @filename   :   main.cpp
- #  @brief      :   2.13inch e-paper display (B) demo
- #  @author     :   Yehui from Waveshare
- #
- #  Copyright (C) Waveshare     August 15 2017
- #
- # Permission is hereby granted, free of charge, to any person obtaining a copy
- # of this software and associated documnetation files (the "Software"), to deal
- # in the Software without restriction, including without limitation the rights
- # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- # copies of the Software, and to permit persons to  whom the Software is
- # furished to do so, subject to the following conditions:
- #
- # The above copyright notice and this permission notice shall be included in
- # all copies or substantial portions of the Software.
- #
- # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- # FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- # LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- # THE SOFTWARE.
- ##
 from __future__ import print_function
 
 import epd2in13b
+import sched, time
+
 from PIL import Image, ImageFont, ImageDraw
 
 from urllib.parse import urlparse
@@ -48,5 +25,12 @@ def main():
     BirthdayWriter().write(epd, frame_black, frame_red)
     epd.display_frame(frame_black, frame_red)
 
+def periodic(scheduler, interval, action, actionargs=()):
+    scheduler.enter(interval, 1, periodic,
+                    (scheduler, interval, action, actionargs))
+    action(*actionargs)
+
 if __name__ == '__main__':
-    main()
+    scheduler = sched.scheduler(time.time, time.sleep)
+    # Refresh every minutes
+    periodic(scheduler, 60, main)

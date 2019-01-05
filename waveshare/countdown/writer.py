334601ffa1ac997055c3334cdee370b369d57001
Théry Charasson <thery.charasson@gmail.com>
2019-01-05 13:39:22 +0100

    Update writer.py

--- waveshare/countdown/writer.py
+++ waveshare/countdown/writer.py
@@ -8,12 +8,13 @@ from PIL import ImageFont
 class CountdownWriter(InkWriter):
 
     config = {}
+    fontName = '/usr/share/fonts/truetype/freefont/FreeMono.ttf';
 
     def __init__(self, config):
         self.config = config
 
     def write(self, epd, frame_black, frame_highlight):
-        font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', 25)
+        font = ImageFont.truetype(fontName, 25)
 
         http_obj = Http()
         resp, content = http_obj.request(uri=self.config.get_countdown_uri(), method='GET')
@@ -23,7 +24,15 @@ class CountdownWriter(InkWriter):
         epd.draw_string_horizontal_at(frame_black, 10, 10, "{0}".format(content["type"]), font, InkWriter.COLORED)
 
         # Write event name
-        epd.draw_string_horizontal_at(frame_highlight, 10, 30, "{0}".format(content["name"]), font, InkWriter.COLORED)
+        nameFontSize = 25;
+        if(len(name) > 15) {
+            font = ImageFont.truetype(fontName, 15)
+            if(len(name) > 30) {
+                name = name[:29] + "..."
+            }
+        }
+        nameFont = ImageFont.truetype(fontName, nameFontSize)
+        epd.draw_string_horizontal_at(frame_highlight, 10, 30, "{0}".format(content["name"]), nameFont, InkWriter.COLORED)
 
         # Write days
         epd.draw_string_horizontal_at(frame_black, 10, 50, "{0} DAYS".format(content["days"]), font, InkWriter.COLORED)

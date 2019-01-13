105e5445f9215fc2d61083739a8cf5a5c62fc2fe
Théry Charasson <thery.charasson@gmail.com>
2019-01-13 09:43:28 +0100

    Update image_writer.py

--- waveshare/image_writer.py
+++ waveshare/image_writer.py
@@ -6,5 +6,5 @@ from PIL import Image
 class ImageWriter(InkWriter):
 
     def write(self, epd, frame_black, frame_highlight):
-        frame_black = epd.get_frame_buffer(Image.open('demo/black.bmp'))
-        frame_highlight = epd.get_frame_buffer(Image.open('demo/highlight.bmp'))
+        frame_black[:] = epd.get_frame_buffer(Image.open('demo/black.bmp'))
+        frame_highlight[:] = epd.get_frame_buffer(Image.open('demo/highlight.bmp'))
